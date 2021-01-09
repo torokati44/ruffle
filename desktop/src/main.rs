606575813133607a82a3ef3a126964a7e74cb51e
David Wendt <dcrkid@yahoo.com>
2021-01-08 19:59:12 -0500

    chore: More clippy compliance

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -17,7 +17,6 @@ use isahc::{config::RedirectPolicy, prelude::*, HttpClient};
 use ruffle_core::{
     backend::audio::{AudioBackend, NullAudioBackend},
     config::Letterbox,
-    backend::video::NullVideoBackend,
     Player,
 };
 use ruffle_render_wgpu::WgpuRenderBackend;
