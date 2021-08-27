1c5452f2125ebad55a497422c6fb50562900f434
TÖRÖK Attila <torokati44@gmail.com>
2021-08-27 19:05:30 +0200

    bench: automatically print per-loop speedups relative to a hardcoded baseline

--- web/bench/run.py
+++ web/bench/run.py
@@ -1,6 +1,6 @@
 """
 
-python3 -m pip install --user playwright matplotlib wget
+python3 -m pip install --user playwright matplotlib wget numpy
 python3 -m playwright install
 python3 ./run.py
 
@@ -47,19 +47,33 @@ def print_args(msg):
                     print("finished")
                     finished = True
 
+baselines = {
+    37 : 7.7692,
+    7311 : 41.8308,
+    4145 : 21.8407,
+    437 : 38.5978,
+    1650 : 6.4385,
+    2139 : 13.1418,
+    4023 : 9.5341,
+    3664 : 45.7593,
+    3946 : 42.4044,
+    4449 : 43.7198,
+    7081 : 44.2165,
+    7711 : 42.444,
+}
 
-loops = ["37", "7311", "4145", "437", "1650", "2139", "4023", "3664", "3946", "4449", "7081", "7711"]
 data = {}
-for l in loops:
+for l in baselines:
    data[l] = []
 
 with sync_playwright() as p:
-    # TODO: unset MOZ_X11_EGL from env ?
-    for l in loops:
+    for l in baselines:
         cur_loop = l
 
-        if not os.path.exists("z0r-de_" + l + ".swf"):
-            wget.download("https://z0r.de/L/z0r-de_" + l + ".swf")
+        if not os.path.exists("z0r-de_" + str(l) + ".swf"):
+            url = "https://z0r.de/L/z0r-de_" + str(l) + ".swf"
+            print("Downloading", url)
+            wget.download(url)
 
         browser = p.chromium.launch(headless=False) #, firefox_user_prefs= {"webgl.force-enabled": True})
 
@@ -68,7 +82,7 @@ with sync_playwright() as p:
 
         page = browser.new_page()
         page.on("console", print_args)
-        page.goto("http://localhost:" + str(PORT) + "/?loop=" + l)
+        page.goto("http://localhost:" + str(PORT) + "/?loop=" + str(l))
         page.wait_for_timeout(1000)
         page.click("#play_button")
         started = True
@@ -78,10 +92,20 @@ with sync_playwright() as p:
         browser.close()
 
 
+speedups = []
+
+print("AVG run_frame duration for loops (excluding the first few frames):")
 for d in data:
     plt.plot(data[d], label=d)
-    print("AVG run_frame duration for loop", d, "=", np.mean(data[d][10:]), "ms")
+
+    avg = np.mean(data[d][10:])
+    speedup = avg / baselines[d]
+    speedups.append(speedup)
+
+    print("   ", d, ":", round(avg, 4), "\t# in ms,", str(round(speedup * 100.0, 2)) + "%% of baseline")
+
 print("(excluding the first few frames)")
+print("overall speedup:", str(round((np.mean(speedups) - 1.0) * 100.0, 2)) + "%%")
 
 plt.ylim(bottom=0)
 plt.grid()
